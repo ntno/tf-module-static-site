76ddc77e5d0207b0d933faa8ace4bddab193beaa
ntno <ntno@users.noreply.github.com>
2022-10-16 15:18:06 -0400

    wip

--- cloudfront.tf
+++ cloudfront.tf
@@ -1,7 +1,7 @@
 resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   origin {
     domain_name = var.domain_name
-    origin_id   = aws_s3_bucket_website_configuration.s3_bucket_website_configuration.website_endpoint
+    origin_id   = aws_s3_bucket_website_configuration.s3_content_bucket_website_configuration.website_endpoint
     custom_origin_config {
       http_port                = 80
       https_port               = 443
@@ -24,7 +24,7 @@ resource "aws_cloudfront_distribution" "cloudfront_distribution" {
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = aws_s3_bucket_website_configuration.s3_bucket_website_configuration.website_endpoint
+    target_origin_id = aws_s3_bucket_website_configuration.s3_content_bucket_website_configuration.website_endpoint
     compress         = true
 
     viewer_protocol_policy = "redirect-to-https"
